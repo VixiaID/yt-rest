9c3cea776d5b5a6dddec346a7632ab62e7eac870
OTSEE <76693678+VixiaID@users.noreply.github.com>
2022-04-11 13:11:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var beautify = require("json-beautify");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-var formatThousandsRegExp=/\B(?=(\d{3})+(?!\d))/g,formatDecimalsRegExp=/(?:\.0*|(\.[^0]+)0+)$/,map={b:1,kb:1024,mb:1&lt;&lt;20,gb:1&lt;&lt;30,tb:Math.pow(1024,4),pb:Math.pow(1024,5)},parseRegExp=/^((-|\+)?(\d+(?:\.\d+)?)) *(kb|mb|gb|tb|pb)$/i;function bytes(a,e){return"string"==typeof a?parse(a):"number"==typeof a?format(a,e):null}function format(a,e){if(!Number.isFinite(a))return null;var r=Math.abs(a),t=e&amp;&amp;e.thousandsSeparator||"",p=e&amp;&amp;e.unitSeparator||"",o=e&amp;&amp;void 0!==e.decimalPlaces?e.decimalPlaces:2,n=Boolean(e&amp;&amp;e.fixedDecimals),m=e&amp;&amp;e.unit||"";m&amp;&amp;map[m.toLowerCase()]||(m=r&gt;=map.pb?"PB":r&gt;=map.tb?"TB":r&gt;=map.gb?"GB":r&gt;=map.mb?"MB":r&gt;=map.kb?"KB":"B");var s=(a/map[m.toLowerCase()]).toFixed(o);return n||(s=s.replace(formatDecimalsRegExp,"$1")),t&amp;&amp;(s=s.replace(formatThousandsRegExp,t)),s+p+" "+m}function parse(a){if("number"==typeof a&amp;&amp;!isNaN(a))return a;if("string"!=typeof a)return null;var e,r=parseRegExp.exec(a),t="b";return r?(e=parseFloat(r[1]),t=r[4].toLowerCase()):(e=parseInt(a,10),t="b"),Math.floor(map[t]*e)}
+var formatThousandsRegExp=/\B(?=(\d{3})+(?!\d))/g,formatDecimalsRegExp=/(?:\.0*|(\.[^0]+)0+)$/,map={b:1,kb:1024,mb:1<<20,gb:1<<30,tb:Math.pow(1024,4),pb:Math.pow(1024,5)},parseRegExp=/^((-|\+)?(\d+(?:\.\d+)?)) *(kb|mb|gb|tb|pb)$/i;function bytes(a,e){return"string"==typeof a?parse(a):"number"==typeof a?format(a,e):null}function format(a,e){if(!Number.isFinite(a))return null;var r=Math.abs(a),t=e&&e.thousandsSeparator||"",p=e&&e.unitSeparator||"",o=e&&void 0!==e.decimalPlaces?e.decimalPlaces:2,n=Boolean(e&&e.fixedDecimals),m=e&&e.unit||"";m&&map[m.toLowerCase()]||(m=r>=map.pb?"PB":r>=map.tb?"TB":r>=map.gb?"GB":r>=map.mb?"MB":r>=map.kb?"KB":"B");var s=(a/map[m.toLowerCase()]).toFixed(o);return n||(s=s.replace(formatDecimalsRegExp,"$1")),t&&(s=s.replace(formatThousandsRegExp,t)),s+p+" "+m}function parse(a){if("number"==typeof a&&!isNaN(a))return a;if("string"!=typeof a)return null;var e,r=parseRegExp.exec(a),t="b";return r?(e=parseFloat(r[1]),t=r[4].toLowerCase()):(e=parseInt(a,10),t="b"),Math.floor(map[t]*e)}
 
 http.createServer(function (req, res) {
     res.writeHead(200, {
         "Access-Control-Allow-Origin": "*",
         "content-type": "text/plain"
     });
-    if (req.url.split("/?get=")[1] == undefined == false &amp;&amp; isUrl(req.url.split("/?get=")[1]) == true &amp;&amp; req.method === "GET") {
+    if (req.url.split("/?get=")[1] == undefined == false && isUrl(req.url.split("/?get=")[1]) == true && req.method === "GET") {
       let linkYt=req.url.split("/?get=")[1];
       unirest('GET',linkYt)
       .end(function (resku) {
@@ -20,13 +20,13 @@ http.createServer(function (req, res) {
         let dbSc=dom.window.document.querySelectorAll("script");
         let dbScript=[];
         dbSc.forEach(function(a){
-          if(a.innerHTML.indexOf("ytInitialPlayerResponse")&gt;0){
+          if(a.innerHTML.indexOf("ytInitialPlayerResponse")>0){
             dbScript.push(a.innerHTML);
           };
         });
         let fixScript="";
         dbScript.forEach(function(b){
-          if(b.indexOf("streamingData")&gt;0){
+          if(b.indexOf("streamingData")>0){
             fixScript=b;
           };
         });
@@ -40,7 +40,7 @@ http.createServer(function (req, res) {
           };
           dbFormat.forEach(function(c){
             var formatData={};
-            formatData["url"]=c.url+"&amp;title="+nameFile;
+            formatData["url"]=c.url+"&title="+nameFile;
             formatData["mime"]=c.mimeType;
             formatData["size"]=bytes(Number(c.contentLength));
             if(c.fps){
